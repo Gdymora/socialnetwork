a743be603bc83413e83d6722774298df8cae199a
Alexandr <alekspetrofint@gmail.com>
2023-10-23 19:31:03 +0300

    style sevtion_1

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
 
 themeToggle.addEventListener("click", () => {
-    body.classList.toggle("dark-theme");
+  body.classList.toggle("dark-theme");
 });
+
+//
+// Отримання посилань на елементи кнопки "Меню" та мобільного меню
+const mobileMenuButton = document.getElementById("mobile-menu-button"); 
+const mobileNav = document.querySelector('.menu');
+// Функція для відкриття/закриття мобільного меню
+function toggleMobileMenu() {
+    mobileNav.classList.toggle('open');
+}
+
+// Додавання обробника подій для кнопки "Меню"
+mobileMenuButton.addEventListener("click", toggleMobileMenu);
