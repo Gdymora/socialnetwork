0b3f0f02611713d4a3d22817bf6a0e911bb7731a
Alexandr <alekspetrofint@gmail.com>
2024-02-02 18:02:51 +0200

    add route

--- cabinet/resources/js/Pages/Dashboard/Partials/LeftSidebar/SuggestionsList.tsx
+++ cabinet/resources/js/Pages/Dashboard/Partials/LeftSidebar/SuggestionsList.tsx
@@ -1,6 +1,11 @@
-import {  RandomUserForFriendship } from "@/types";
+import { RandomUserForFriendship } from "@/types";
+import { Inertia } from "@inertiajs/inertia";
 
-export function SuggestionsList({ randomUsersForFriendship }: { randomUsersForFriendship: RandomUserForFriendship[] }) {
+export function SuggestionsList({
+    randomUsersForFriendship,
+}: {
+    randomUsersForFriendship: RandomUserForFriendship[];
+}) {
     return (
         <div className="section_2">
             <div className="header-card">
@@ -14,44 +19,57 @@ export function SuggestionsList({ randomUsersForFriendship }: { randomUsersForFr
             <div className="section centered-container">
                 <div className="border-block-end"></div>
                 {/* Map over the users array to render each user */}
-                {randomUsersForFriendship.map((friend:RandomUserForFriendship ) => (
-                    <div
-                        className="item_left_sidebar_section_2"
-                        key={friend.id}
-                    >
-                        {" "}
-                        {/* Ensure friend has a unique 'id' property */}
-                        <div className="circle">
-                            {/* Use a placeholder image if profile_image_url is not available */}
-                            <img
-                                src={friend.profile_image_url
-                                    ? `/user-file/${friend.profile_image_url}`
-                                    : "/assets/images/noimg.png"
-                                }
-                                alt={`${friend.name} ${friend.last_name}`}
-                            />
-                        </div>
-                        <div className="text">
-                            <p className="bold">
-                                {friend.name} {friend.last_name}
-                            </p>
-                            {/* Check if aboutMe and occupations exist before rendering */}
-                            {friend.about_me && friend.about_me.occupations && (
-                                <p className="text-light">
-                                    {friend.about_me.occupations}
+                {randomUsersForFriendship.map(
+                    (friend: RandomUserForFriendship) => (
+                        <div
+                            className="item_left_sidebar_section_2"
+                            key={friend.id}
+                        >
+                            {" "}
+                            {/* Ensure friend has a unique 'id' property */}
+                            <div className="circle">
+                                {/* Use a placeholder image if profile_image_url is not available */}
+                                <img
+                                    src={
+                                        friend.profile_image_url
+                                            ? `/user-file/${friend.profile_image_url}`
+                                            : "/assets/images/noimg.png"
+                                    }
+                                    alt={`${friend.name} ${friend.last_name}`}
+                                />
+                            </div>
+                            <div className="text">
+                                <p className="bold">
+                                    {friend.name} {friend.last_name}
                                 </p>
-                            )}
+                                {/* Check if aboutMe and occupations exist before rendering */}
+                                {friend.about_me &&
+                                    friend.about_me.occupations && (
+                                        <p className="text-light">
+                                            {friend.about_me.occupations}
+                                        </p>
+                                    )}
+                            </div>
+                            <div className="flex">
+                                <button
+                                    onClick={() =>
+                                        Inertia.visit(
+                                            route("profile-friend", {
+                                                id: friend.id,
+                                            })
+                                        )
+                                    }
+                                    className="button-icon right-top"
+                                >
+                                    <i
+                                        className="bi bi-plus-square"
+                                        style={{ fontSize: "25px" }}
+                                    ></i>
+                                </button>
+                            </div>
                         </div>
-                        <div className="flex">
-                            <button className="button-icon right-top">
-                                <i
-                                    className="bi bi-plus-square"
-                                    style={{ fontSize: "25px" }}
-                                ></i>
-                            </button>
-                        </div>
-                    </div>
-                ))}
+                    )
+                )}
                 <div>
                     <a href="#">View More Profiles</a>
                 </div>{" "}
--- cabinet/resources/js/app.tsx
+++ cabinet/resources/js/app.tsx
@@ -1,29 +1,36 @@
-import './bootstrap';
-import '../css/app.css';
-import '../css/style.css';
-import '../css/galery.css';
-import '../css/profile.css';
-import '../css/users.css';
-import '../css/workshop.css';
-import '../css/audioAndVideo.css';
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import "./bootstrap";
+import "../css/app.css";
+import "../css/style.css";
+import "../css/galery.css";
+import "../css/profile.css";
+import "../css/users.css";
+import "../css/workshop.css";
+import "../css/audioAndVideo.css";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { BrowserRouter as Router } from "react-router-dom";
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
-        const parts = name.split('/');
-        const path = parts.length > 1 ?  `${name}`: `${name}/${name}`;
-        return resolvePageComponent(`./Pages/${path}.tsx`, import.meta.glob('./Pages/**/*.tsx'));
+        const parts = name.split("/");
+        const path = parts.length > 1 ? `${name}` : `${name}/${name}`;
+        return resolvePageComponent(
+            `./Pages/${path}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        );
     },
     setup({ el, App, props }) {
         const root = createRoot(el);
-        root.render(<App {...props} />);
+        root.render(
+            <Router>
+                <App {...props} />
+            </Router>
+        );
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
